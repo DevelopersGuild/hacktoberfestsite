f354435dbd284a1829fe0db3c3fcd110791e95d9
yousefh409 <57015880+yousefh409@users.noreply.github.com>
2020-10-21 08:50:00 -0700

    Update navigation-bar.jsx

--- src/components/navigation-bar.jsx
+++ src/components/navigation-bar.jsx
@@ -75,13 +75,13 @@ export default class NavigationBar extends React.Component {
                 <MenuItem onClick={this.handleClose}>Schedule</MenuItem>
               </Link>
 
-              <Link spy={true} smooth={true} duration={500} offset={-70}>
+              <Link to="#speakers" spy={true} smooth={true} duration={500} offset={-70}>
                 <MenuItem onClick={this.handleClose}>Speakers</MenuItem>
               </Link>
 
               <Link to="#team" spy={true} smooth={true} duration={500} offset={-70}>
                 <MenuItem onClick={this.handleClose}>Team</MenuItem>
-              </Link>
+              </Link>to="#schedule" 
 
               <Link to="#faq" spy={true} smooth={true} duration={500} offset={-70}>
                 <MenuItem onClick={this.handleClose}>FAQ</MenuItem>
